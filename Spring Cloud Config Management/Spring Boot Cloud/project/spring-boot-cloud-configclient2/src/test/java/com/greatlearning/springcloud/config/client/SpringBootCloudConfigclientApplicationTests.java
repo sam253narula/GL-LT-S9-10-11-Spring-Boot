0534f28fedd0c4c2587a16328d304a5e97f06796
sam253narula <samarthnarula253@gmail.com>
2021-09-04 12:15:56 +0530

    adding Spring Cloud COnfig  Management Project

---
+++ Spring Cloud Config Management/Spring Boot Cloud/project/spring-boot-cloud-configclient2/src/test/java/com/greatlearning/springcloud/config/client/SpringBootCloudConfigclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greatlearning.springcloud.config.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCloudConfigclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
